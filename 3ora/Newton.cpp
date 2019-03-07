eaacafd1f0d7e2cad2c0dad624aee80162f4be05
tamkrisz <tamkrisz90@gmail.com>
2019-03-07 12:09:34 +0100

    Update Newton.cpp

--- 3ora/Newton.cpp
+++ 3ora/Newton.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <cmath>
 
-double func1(double x, double num)
+double func1(double x)
 {
-  return x*x - num;
+  return x*x - 1651465.0;
 }
 
 double func2(double x)
@@ -11,25 +11,21 @@ double func2(double x)
   return 2*x;
 }
 
-template<typename T, typename F1, typename F2>
-T Newton(F1 f1, F2 f2, T x0, T num)
+template<typename F1, typename F2, typename T>
+T Newton(F1 f1, F2 f2, T x0)
 {
-  double x1 = 0; 
-  double temp = 0;
-  std::cout << "x0= " << x0 << std::endl;
-  int i = 1;
-  while(abs(temp-x0) > 0.0001)
+  //std::cout << "x0= " << x0 << std::endl;
+  while(abs(f1(x0)/f2(x0)) > 0.00001)
   {
-    temp = x0;
-    x1 = x0 - f1(x0,num)/f2(x0);
-    x0 = x1;
-    std::cout << temp << std::endl;
+    x0 -= f1(x0)/f2(x0);
+    //std::cout << x0 << std::endl;
   } 
-  return x1;
+  return x0;
 }
 
 int main()
 {
-  std::cout << Newton(func1, func2, 10.0, 612.0);
+  //std::cout << Newton(func1, func2, 10.0); 
+  std::cout << Newton([](double x){ return x*x - 612.0; }, [](double x){ return 2.0*x; }, 10.0);
   return 0;
 }
