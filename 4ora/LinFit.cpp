b82cd1993bf28e2244e8612e59ce37422380d574
tamkrisz <tamkrisz90@gmail.com>
2019-03-12 23:53:30 +0100

    Update LinFit.cpp

--- 4ora/LinFit.cpp
+++ 4ora/LinFit.cpp
@@ -9,7 +9,7 @@ auto f = [](double a, double b)
     return a + std::pow(b,2);
 };
 
-void LinFit(std::vector<double> X, std::vector<double> Y, std::array<double, 2> &result)
+std::array<double, 2> LinFit(std::vector<double> X, std::vector<double> Y, std::array<double, 2> result)
 {
     const double avgX = std::accumulate(X.begin(), X.end(), 0.0) / X.size();
     const double avgY = std::accumulate(Y.begin(), Y.end(), 0.0) / Y.size();
@@ -19,19 +19,20 @@ void LinFit(std::vector<double> X, std::vector<double> Y, std::array<double, 2> 
     double m = avgY - b*avgX;
     result[0] = b; //meredekség
     result[1] = m; //tengelymetszet
+    return result;
 } 
+
 
 int main()
 {
-    std::vector<double> X = {1,2,3,4,5,7,10};
-    std::vector<double> Y2 = {3.11,4.92,7.05,9.20,11.01,15.4,20.97};
-    std::vector<double> Y = {3,5,7,9,11,15,21};
-    //m = 2, b = 1
-    
+    const std::vector<double> X = {1,2,3,4,5,7,10};
+    const std::vector<double> Y2 = {3.11,4.92,7.05,9.20,11.01,15.4,20.97};
+    const std::vector<double> Y = {3,5,7,9,11,15,21};
+
     std::array<double, 2> result;
 
-    LinFit(X,Y,result);
+    result = LinFit(X,Y,result);
     std::cout << result[0] << " " << result[1] << std::endl;
-    LinFit(X,Y2,result);
+    result = LinFit(X,Y2,result);
     std::cout << result[0] << " " << result[1] << std::endl;
 }
