957ed9c987e9adb08e0d7f36d881e8fa873f88b5
tamkrisz <tamkrisz90@gmail.com>
2019-03-13 13:12:18 +0100

    Update LinFit.cpp

--- 4ora/LinFit.cpp
+++ 4ora/LinFit.cpp
@@ -22,17 +22,36 @@ std::array<double, 2> LinFit(std::vector<double> X, std::vector<double> Y, std::
     return result;
 };
 
+double R2(std::vector<double> X, std::vector<double> Y, std::array<double, 2> result)
+{
+    const double avgY = std::accumulate(Y.begin(), Y.end(), 0.0) / Y.size();
+    std::vector<double> Y_orig = Y;
+    //sum(y[i] - avgY)^2
+    std::for_each(Y.begin(), Y.end(), [&](double& d) { d-=avgY;});
+    const double Stot = std::accumulate(Y.begin(), Y.end(), 0.0, f);
+
+    //sum(y[i] - f[i])^2;
+    for(int i=0;i<X.size();i++)
+    {
+        Y_orig[i] -= result[0]*X[i] + result[1];
+    }
+    const double Sres = std::accumulate(Y_orig.begin(), Y_orig.end(), 0.0, f);
+
+    //R^2
+    const double R2 = 1 - Sres/Stot;
+    return R2;
+};
+
 
 int main()
 {
     const std::vector<double> X = {1,2,3,4,5,7,10};
-    const std::vector<double> Y2 = {3.11,4.92,7.05,9.20,11.01,15.4,20.97};
+    const std::vector<double> Y2 = {3.11,4.92,7.05,9.2,11.01,15.4,20.97};
+    const std::vector<double> Y3 = {3.3,4.6,7,9.50,11.51,15.5,20.8};
     const std::vector<double> Y = {3,5,7,9,11,15,21};
 
-    std::array<double, 2> result;
-
-    result = LinFit(X,Y,result);
-    std::cout << result[0] << " " << result[1] << std::endl;
-    result = LinFit(X,Y2,result);
-    std::cout << result[0] << " " << result[1] << std::endl;
+    std::array<double, 2> result = LinFit(X,Y3,result);
+    std::cout << "Meredekseg= " << result[0] << std::endl;
+    std::cout << "Tengelymetszet= " << result[1] << std::endl;
+    std::cout << "R^2= " << R2(X,Y3,result) << std::endl;
 }
