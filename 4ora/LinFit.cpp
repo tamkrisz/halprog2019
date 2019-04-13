1ec7aebc7ced294284a8e4ddda036748561db859
tamkrisz <tamkrisz90@gmail.com>
2019-04-14 01:36:48 +0200

    Update LinFit.cpp

--- 4ora/LinFit.cpp
+++ 4ora/LinFit.cpp
@@ -3,40 +3,42 @@
 #include <array>
 #include <numeric>
 
+double func (double x, double y) {return x-10;}
+double func2 (double x, double y) {return y-10;}
 
 auto f = [](double a, double b)
 {
-    return a + std::pow(b,2);
+    return a + b*b;
 };
 
-std::array<double, 2> LinFit(std::vector<double> X, std::vector<double> Y, std::array<double, 2> result)
+std::array<double, 2> LinFit(std::vector<double> const& X, std::vector<double> const& Y)
 {
     const double avgX = std::accumulate(X.begin(), X.end(), 0.0) / X.size();
     const double avgY = std::accumulate(Y.begin(), Y.end(), 0.0) / Y.size();
-    std::for_each(X.begin(), X.end(), [&](double& d) { d-=avgX;});
-    std::for_each(Y.begin(), Y.end(), [&](double& d) { d-=avgY;});
-    double b = std::inner_product(X.begin(), X.end(), Y.begin(), 0.0) / std::accumulate(X.begin(), X.end(), 0.0, f);
-    double m = avgY - b*avgX;
-    result[0] = b; //meredekség
-    result[1] = m; //tengelymetszet
+
+    double b = 0;
+    double temp = 0;
+    for(int i=0;i<X.size();i++)
+    {
+        b += (X[i]-avgX) * (Y[i] - avgY);
+        temp += (X[i]-avgX) * (X[i]-avgX);
+    }
+    b /= temp;
+    const double m = avgY - b*avgX;
+    std::array<double, 2> result = {b, m};
     return result;
 };
 
-double R2(std::vector<double> X, std::vector<double> Y, std::array<double, 2> result)
+double R2(std::vector<double> const& X, std::vector<double> const& Y, std::array<double, 2> &result)
 {
     const double avgY = std::accumulate(Y.begin(), Y.end(), 0.0) / Y.size();
-    std::vector<double> Y_orig = Y;
-    //sum(y[i] - avgY)^2
-    std::for_each(Y.begin(), Y.end(), [&](double& d) { d-=avgY;});
-    const double Stot = std::accumulate(Y.begin(), Y.end(), 0.0, f);
-
-    //sum(y[i] - f[i])^2;
+    double Stot = 0;
+    double Sres = 0;
     for(int i=0;i<X.size();i++)
     {
-        Y_orig[i] -= result[0]*X[i] + result[1];
+        Stot += (Y[i]-avgY) * (Y[i]-avgY);
+        Sres += (Y[i] - result[0]*X[i] - result[1]) * (Y[i] - result[0]*X[i] - result[1]);
     }
-    const double Sres = std::accumulate(Y_orig.begin(), Y_orig.end(), 0.0, f);
-
     //R^2
     const double R2 = 1 - Sres/Stot;
     return R2;
@@ -46,12 +48,12 @@ double R2(std::vector<double> X, std::vector<double> Y, std::array<double, 2> re
 int main()
 {
     const std::vector<double> X = {1,2,3,4,5,7,10};
-    const std::vector<double> Y2 = {3.11,4.92,7.05,9.2,11.01,15.4,20.97};
-    const std::vector<double> Y3 = {3.3,4.6,7,9.50,11.51,15.5,20.8};
     const std::vector<double> Y = {3,5,7,9,11,15,21};
 
-    std::array<double, 2> result = LinFit(X,Y3,result);
+    std::array<double, 2> result = LinFit(X,Y);
+    //elvárt eredmények: meredkség=2, tengelymetszet=1, r^2 = 1
+    //használt calculator: https://ncalculators.com/statistics/r-squared-calculator.htm
     std::cout << "Meredekseg= " << result[0] << std::endl;
     std::cout << "Tengelymetszet= " << result[1] << std::endl;
-    std::cout << "R^2= " << R2(X,Y3,result) << std::endl;
+    std::cout << "R^2= " << R2(X,Y,result) << std::endl;
 }
