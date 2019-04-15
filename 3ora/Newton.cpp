f1f53686247deab1171f68c8430147d2724bd6d7
tamkrisz <tamkrisz90@gmail.com>
2019-04-15 13:50:53 +0200

    Update Newton.cpp

--- 3ora/Newton.cpp
+++ 3ora/Newton.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
 #include <cmath>
 
-double func1(double x)
-{
-  return x*x - 1651465.0;
-}
-
-double func2(double x)
-{
-  return 2*x;
-}
-
 template<typename F1, typename F2, typename T>
 T Newton(F1 f1, F2 f2, T x0)
 {
-  T diff = f1(x0)/f2(x0);
-  while(abs(diff) > 0.00001)
+  T diff = 1;
+  while(std::abs(diff) > 0.00001)
   {
     diff = f1(x0)/f2(x0);
     x0 -= diff;
@@ -25,7 +15,7 @@ T Newton(F1 f1, F2 f2, T x0)
 
 int main()
 {
-  //std::cout << Newton(func1, func2, 10.0); 
+  std::cout.precision(16);
   std::cout << Newton([](double x){ return x*x - 612.0; }, [](double x){ return 2.0*x; }, 10.0);
   return 0;
 }
