7740ffeff2127b0a8afcf37a9d206553627a389f
tamkrisz <tamkrisz90@gmail.com>
2019-04-21 20:57:07 +0200

    Update random_time.cpp

--- 6ora/random_time.cpp
+++ 6ora/random_time.cpp
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <cmath>
 
+
+//az illesztett függvény: f(x) = 1.0991e-05*(x-484.79)^3
 int main()
 {   
 
@@ -14,13 +16,12 @@ int main()
     myfile.open("matmul_time.txt");
     
     std::mt19937 gen(1);
-    std::normal_distribution<double> distr(1000,200);
+    std::normal_distribution<double> distr(10,5);
 
-    double time_elapsed = 0;
+    int time_elapsed = 0;
     double min = 1000000;
-    double max = 0;
     int N = 100;
-    std::vector<int> sizes = {500,1000,1500,2000};
+    std::vector<int> sizes = {500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400};
     for(int l=0;l<sizes.size();l++)
     {
         N = sizes[l];
@@ -39,11 +40,10 @@ int main()
             auto t1 = std::chrono::high_resolution_clock::now();
             time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0).count();
             if(time_elapsed < min) min = time_elapsed;
-            if(time_elapsed > max) max = time_elapsed;
         }
-        
-        myfile << N << " " << min << " " << max << std::endl;
-        std::cout << N << " " << min << " " << max << std::endl;
+        note
+        myfile << N << " " << min << " " << std::endl;
+        std::cout << N << " " << min << " " << std::endl;
         min = 1000000;
     }
 }
