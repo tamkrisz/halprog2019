c926908ea1f1bb821df3ca08ac798f5721d0c144
tamkrisz <tamkrisz90@gmail.com>
2019-04-21 20:07:40 +0200

    Update main.cpp

--- 5ora/main.cpp
+++ 5ora/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <cmath> 
 #include "vec.h"
+#include <sstream>
+#include <string>
 
 
-int main() {
+
+int main()
+{
+	auto err = [](auto str){ std::cout << "Matrix.h error in: " << str << "\n"; std::exit(-1); };
+
     Vector2d<double> V1 = {3.0, 4.0};
     Vector2d<double> V2 = {1.5, 2.5};
     Vector2d<double> V1copy = {3.0, 4.0};
@@ -46,7 +52,7 @@ int main() {
 
 
     
-    //+= test
+
     V1+=V2;
     if((std::abs(V1.x - V1addV2res.x) > diff) || (std::abs(V1.y - V1addV2res.y) >diff)) std::cout << "+=operator failed" << std::endl;
     //-= test
@@ -54,21 +60,68 @@ int main() {
     if((std::abs(V1copy.x - V1subV2res.x) > diff) || (std::abs(V1copy.y - V1subV2res.y) > diff)) std::cout << "-=operator failed" << std::endl;
     
     auto V1old = V1;
-    V1*=10;
-    if(std::abs(10*V1old.x -V1.x) > diff || std::abs(10*V1old.y - V1.y) > diff) std::cout << "*= scalar failed" << std::endl;
-    V1/=10;
-    if(std::abs(V1old.x -V1.x) > diff || std::abs(V1old.y - V1.y) > diff) std::cout << "/= scalar failed" << std::endl;
+
+    // *=
+    {
+        Vector2d<double> a = {1.0, 2.0};
+        Vector2d<double> b = {10.0, 20.0};
+        a*=10.0;
+        if(std::abs(a.x - b.x) > diff || std::abs(a.y - b.y) > diff) std::cout << "*= scalar failed" << std::endl;
+    }
+    // /=
+    {
+        Vector2d<double> a = {1.0, 2.0};
+        Vector2d<double> b = {10.0, 20.0};
+        b/=10.0;
+        if(std::abs(a.x - b.x) > diff || std::abs(a.y - b.y) > diff) std::cout << "*= scalar failed" << std::endl;
+    }
+
 
     //* operator
-    Vector2d<double> c = {1.0,2.0};
-    Vector2d<double> d = 10.0*c;
-    if(std::abs(10*c.x - d.x) > diff || std::abs(10*c.y - d.y) > diff) std::cout << "* scalar failed" << std::endl;
-    // /operator
-    d = c/10.0;
-    if(std::abs(c.x/10 - d.x) > diff || std::abs(c.y/10 - d.y) > diff) std::cout << "/ scalar failed" << std::endl;
-    //In/out
-    std::cout << std::endl << "Input/output test:" << std::endl;
-    Vector2d<double> V{};
-    std::cin >> V;
-    std::cout << V;
+    {
+    Vector2d<double> b = {1.0,2.0};
+    Vector2d<double> d = {10.0, 20.0};
+    Vector2d<double> c = 10.0*b;
+    if(std::abs(c.x - d.x) > diff || std::abs(c.y - d.y) > diff) std::cout << "* scalar failed" << std::endl;
+    }
+
+
+    //* operator
+    {
+    Vector2d<double> b = {1.0,2.0};
+    Vector2d<double> d = {10.0, 20.0};
+    Vector2d<double> c = b*10.0;
+    if(std::abs(c.x - d.x) > diff || std::abs(c.y - d.y) > diff) std::cout << "* scalar failed" << std::endl;
+    }
+
+    // / operator
+    {
+    Vector2d<double> d = {1.0,2.0};
+    Vector2d<double> b = {10.0, 20.0};
+    Vector2d<double> c = b/10.0;
+    if(std::abs(c.x - d.x) > diff || std::abs(c.y - d.y) > diff) std::cout << "* scalar failed" << std::endl;
+    }
+
+    // << test
+	{
+		Vector2d<double> ref = {1.0,2.0};
+		std::string ref_string = "1.0 2.0";
+		std::stringstream ss;
+		std::string temp;
+		ss << ref;
+		temp = ss.str();
+		if( !temp.compare(ref_string) ) { err("<< test");        }
+	}
+
+	//>> test
+	{
+		Vector2d<double> ref = {1.0,2.0};
+		Vector2d<double> a;
+		std::stringstream ss;
+		std::string temp;
+		ss << "1.0 2.0";
+		ss >> a;
+		if(a.x != ref.x || a.y != ref.y) { err(">> test [elements]"); }
+	}
+
  } 
