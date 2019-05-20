02ccf06d4bbd99b74b3b1e7eb69483a8984002aa
tamkrisz <tamkrisz90@gmail.com>
2019-05-20 14:19:43 +0200

    Update MCint.h

--- 3D_MC_integral/MCint.h
+++ 3D_MC_integral/MCint.h
@@ -1,39 +1,58 @@
 #include <iostream>
 #include <vector>   
+#include <chrono>  
 #include <algorithm>
 #include <random>
+#include <fstream>
 #include <cmath>
+#include "MCint.h"
 
-template<typename F1, typename F2>
-double MonteCarlo(F1 f1, F2 f2, double x_l, double x_u, double y_l, double y_u, double z_l, double z_u, int N)
+
+void Error()
 {
-    if(x_l > x_u)  { std::cout << "Wrong intervals! X upper limit is smaller than lower limit!" << std::endl; std::exit(-1);  }
-    if(y_l > y_u)  { std::cout << "Wrong intervals! Y upper limit is smaller than lower limit!" << std::endl; std::exit(-1);  }
-    if(z_l > z_u)  { std::cout << "Wrong intervals! Z upper limit is smaller than lower limit!" << std::endl; std::exit(-1);  }
-    
-    double x_value = 0;
-    double y_value = 0;
-    double z_value = 0;
+    std::ofstream myfile;
+    double x = 0;
+    myfile.open("error.txt");
+    double exact_value = 0.3333333333333; //x^2 integrálja 0-tol 1-ig 1/3
+    std::vector<int> sizes = {1000,10000,50000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000};
 
-    std::random_device rd; 
-    std::mt19937 gen(rd());
-
-    std::uniform_real_distribution<double> disX(x_l, x_u);
-    std::uniform_real_distribution<double> disY(y_l, y_u);
-    std::uniform_real_distribution<double> disZ(z_l, z_u);
-
-    const double V0 = (x_u-x_l) * (y_u-y_l) * (z_u-z_l);
-    double integral_value = 0.0;
-
-    for(auto i=0;i<N;i++)
+    for(unsigned int i=0;i<sizes.size();i++)
     {
-        x_value = disX(gen);
-        y_value = disY(gen);
-        z_value = disZ(gen);
-        if(f2(x_value,y_value,z_value)) integral_value += f1(x_value,y_value,z_value);
+        x = MonteCarlo( [](auto x, auto y, auto z){ return x*x; },
+            [](auto x, auto y, auto z)->bool{ return x < 1; },
+            0, 2, 0, 1, 0, 1,
+            sizes[i]);
+        myfile << sizes[i] << " " << x << " " << std::abs(exact_value - x) << std::endl;
+        std::cout << sizes[i] << " " << x << " " << std::abs(exact_value - x) << std::endl;
+    }
+    //az illesztett függvény: f(x) = 0.4478/sqrt(x-8180,59)
+}
+
+
+int main()
+{   
+    std::cout.precision(10);
+     
+    {
+    double result = -57.23958;
+    double x = MonteCarlo( [](auto x, auto y, auto z){ return 1-z; },
+                [](auto x, auto y, auto z)->bool{ return (x*x+y*y+z*z < 5*5 && z > 0 && z < 5); },
+                 0, 5, 0, 5, 0, 5,
+                 10000000); 
+
+    std::cout << std::abs(x - result) << std::endl;
     }
     
-    integral_value *= V0/N;
 
-    return integral_value;
+    {
+    double result = 313.04504;
+    double x = MonteCarlo( [](auto x, auto y, auto z){ return x*x+10*x+2*std::sqrt(y) + std::log(z); },
+                [](auto x, auto y, auto z)->bool{ return 1; },
+                 -1.0, 2.0, 0, 3.0, 0, 4.0,
+                 10000000);
+
+    std::cout << std::abs(x - result) << std::endl;
+    }
+    
+    //Error();
 }
